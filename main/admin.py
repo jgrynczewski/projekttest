bd37cab51c5e3e439a56dfd0ad655942ad362c9a
jgrynczewski <jgrynczewski@gmail.com>
2023-10-20 18:22:33 +0200

    Place and Voyage models

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from main.models import Place, Voyage
+
 # Register your models here.
+admin.site.register(Place)
+admin.site.register(Voyage)
--- main/models.py
+++ main/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
 
 # Create your models here.
+class A(models.Model):
+    ...
+
+
+class Place(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    points = models.DecimalField(max_digits=2 , decimal_places=1, null=True, blank=True)
+    latitude = models.DecimalField(max_digits=6, decimal_places=3)
+    longitude = models.DecimalField(max_digits=6, decimal_places=3)
+    added = models.DateTimeField(auto_now_add=True)
+
+
+class Voyage(models.Model):
+    user = models.ForeignKey(User, models.CASCADE)
+    source = models.ForeignKey(Place, models.SET_NULL, null=True, related_name='as_source')
+    destination = models.ForeignKey(Place, models.SET_NULL, null=True, related_name='as_destination')
